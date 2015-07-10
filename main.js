94a4edfb0b7dc44b5da957797f62af9f652a29d2
Patrick <pcnicklaus@gmail.com>
2015-07-10 16:35:56 -0600

    complete answers

---
+++ main.js
@@ -0,0 +1,37 @@
+
+for (i = 0; i <= 1000; i+=100) {
+  console.log(i);
+}
+
+
+for (i = 1; i <= 1000; i*=2) {
+  console.log(i);
+}
+
+for (i = 0; i <= 10; i+=2) {
+  console.log(i);
+}
+
+for (i = 0; i <= 10; i+=2) {
+  console.log(i);
+}
+
+for (i = 3; i <= 15; i+=3) {
+  console.log(i);
+}
+
+for (i = 9; i > -1; i--) {
+  console.log(i);
+}
+
+for (var i = 1; i <= 4; i++) {
+  for (var j = 1; j <= 3; j++) {
+  console.log(i);
+  }
+  }
+
+  for (var i = 0; i <= 2; i++) {
+  for (var j = 0; j <= 4; j++) {
+  console.log(j);
+  }
+ }
---
+++ readme.md
@@ -0,0 +1,1 @@
+Answers for conditional loops exercise
